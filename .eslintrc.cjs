717f31e84bc430c27de8f9ee9d8643c8b01a7002
Oleksii Bolotin <samkiral73@gmail.com>
2024-07-01 14:19:47 +0200

    chore: add prettier and its configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,15 +4,21 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+    '@typescript-eslint',
+    'prettier'
+],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/no-explicit-any': 'error',
+    'prettier/prettier': 'error',
   },
 }

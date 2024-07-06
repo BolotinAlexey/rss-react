b8d35e5eecbe8415d867a3796c5d3c4ebc0441e1
Oleksii Bolotin <samkiral73@gmail.com>
2024-07-06 22:10:53 +0200

    feat: add local storage and start loading cards

--- src/components/DataView/DataView.tsx
+++ src/components/DataView/DataView.tsx
@@ -13,13 +13,9 @@ export default class DataView extends Component<State, StateDataView> {
     planets: [],
   };
 
-  componentDidUpdate(
-    prevProps: Readonly<State>
-    // prevState: Readonly<StateDataView>
-  ): void {
-    if (prevProps.name !== this.props.name) {
+  componentDidUpdate(prevProps: Readonly<State>): void {
+    if (prevProps.name !== this.props.name || this.props.name === '') {
       getPage(1, this.props.name).then((list) => {
-        console.log(list.results);
         this.setState({ planets: list.results });
       });
     }
@@ -27,15 +23,17 @@ export default class DataView extends Component<State, StateDataView> {
 
   render() {
     return (
-      <ul className="list">
-        {this.state.planets.map((planet: IPlanet) => {
-          return (
-            <li className="list__card" key={planet.name}>
-              <Card {...planet} />
-            </li>
-          );
-        })}
-      </ul>
+      <section className="section section-list">
+        <ul className="list">
+          {this.state.planets.map((planet: IPlanet) => {
+            return (
+              <li className="list__card" key={planet.name}>
+                <Card {...planet} />
+              </li>
+            );
+          })}
+        </ul>
+      </section>
     );
   }
 }
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,6 +1,7 @@
 import { ChangeEvent, Component, FormEvent } from 'react';
 import './form.css';
 import { State } from '../../interfaces';
+import { LS_KEY } from '../../constants/constants';
 
 type Props = {
   onSubmitName: (name: string) => void;
@@ -11,11 +12,17 @@ export default class Form extends Component<Props> {
     name: '',
   };
 
+  componentDidMount(): void {
+    const initWord = localStorage.getItem(LS_KEY) ?? '';
+    this.setState({ name: initWord });
+    this.props.onSubmitName(initWord);
+  }
+
   submitHandler = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const name = this.state.name.replace(/\s+/g, '').toLowerCase();
+    const name = this.state.name.trim().toLowerCase();
     this.props.onSubmitName(name);
-    this.setState({ name: '' });
+    localStorage.setItem(LS_KEY, name);
   };
 
   changeHandler = (event: ChangeEvent<HTMLInputElement>) => {
@@ -24,18 +31,20 @@ export default class Form extends Component<Props> {
 
   render() {
     return (
-      <form className="form" onSubmit={this.submitHandler}>
-        <input
-          className="input"
-          type="text"
-          onChange={this.changeHandler}
-          value={this.state.name}
-          placeholder="Enter name"
-        />
-        <button className="submit-button" type="submit">
-          Search
-        </button>
-      </form>
+      <section className="section section-form">
+        <form className="form" onSubmit={this.submitHandler}>
+          <input
+            className="input"
+            type="text"
+            onChange={this.changeHandler}
+            value={this.state.name}
+            placeholder="Enter name"
+          />
+          <button className="submit-button" type="submit">
+            Search
+          </button>
+        </form>
+      </section>
     );
   }
 }

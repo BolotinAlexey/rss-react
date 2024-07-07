38c5738a9bdcd6d903102d0250b48193d2ab1d5d
Oleksii Bolotin <samkiral73@gmail.com>
2024-07-07 13:28:27 +0200

    feat: add Loader

--- src/components/DataView/DataView.tsx
+++ src/components/DataView/DataView.tsx
@@ -3,36 +3,62 @@ import { IPlanet, State } from '../../interfaces';
 import Card from '../Card';
 import getPage from '../../service/api';
 import './dataView.css';
+import Loader from '../Loader';
 
 interface StateDataView {
   planets: IPlanet[];
+  isLoading: boolean;
 }
 
-export default class DataView extends Component<State, StateDataView> {
+type PropsDataView = State;
+
+export default class DataView extends Component<PropsDataView, StateDataView> {
   state: StateDataView = {
     planets: [],
+    isLoading: false,
   };
 
-  componentDidUpdate(prevProps: Readonly<State>): void {
-    if (prevProps.name !== this.props.name || this.props.name === '') {
-      getPage(1, this.props.name).then((list) => {
-        this.setState({ planets: list.results });
-      });
+  loadPage = async () => {
+    this.setState({ isLoading: true });
+
+    try {
+      const list = await getPage(1, this.props.name);
+      this.setState({ planets: list.results });
+    } catch (error) {
+      console.error('Failed to fetch planets', error);
+    } finally {
+      this.setState({ isLoading: false });
     }
+  };
+
+  componentDidMount(): void {
+    if (this.props.name === '') this.loadPage();
+  }
+
+  async componentDidUpdate(prevProps: Readonly<PropsDataView>): Promise<void> {
+    if (
+      prevProps.name !== this.props.name ||
+      (this.props.name === '' && prevProps.name !== '')
+    )
+      this.loadPage();
   }
 
   render() {
     return (
       <section className="section section-list">
-        <ul className="list">
-          {this.state.planets.map((planet: IPlanet) => {
-            return (
-              <li className="list__card" key={planet.name}>
-                <Card {...planet} />
-              </li>
-            );
-          })}
-        </ul>
+        {this.state.isLoading ? (
+          <Loader />
+        ) : (
+          <ul className="list">
+            {this.state.planets.map((planet: IPlanet) => {
+              return (
+                <li className="list__card" key={planet.name}>
+                  <Card {...planet} />
+                </li>
+              );
+            })}
+          </ul>
+        )}
       </section>
     );
   }

d2bff6dbb29f947c67a82868a72094eb7eafddc2
Oleksii Bolotin <samkiral73@gmail.com>
2024-07-10 22:05:37 +0200

    refactor: change Card component to functional components

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,68 +1,61 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { IPlanet } from '../../interfaces';
 import transformPropsArrayToString from '../../utils/transformPropsArrayToString';
 import './card.css';
 
-interface CardState {
-  filmTitles: string;
-  residentNames: string;
-}
+export default function Card(planet: IPlanet) {
+  const [filmTitles, setFilmTitles] = useState('');
+  const [residentNames, setResidentNames] = useState('');
+  const { url, name, films, residents, created, edited, ...restProps } = planet;
 
-export default class Card extends Component<IPlanet, CardState> {
-  state: CardState = {
-    filmTitles: '',
-    residentNames: '',
+  const transformProps = {
+    ...restProps,
+    created: created.toString().slice(0, 10),
+    edited: edited.toString().slice(0, 10),
   };
 
-  async componentDidMount() {
-    const { films, residents } = this.props;
-    if (films) {
-      const filmTitles = await transformPropsArrayToString(films, 'title');
-      this.setState({ filmTitles });
-    }
-    if (residents) {
-      const residentNames = await transformPropsArrayToString(
-        residents,
-        'name'
-      );
-      this.setState({ residentNames });
-    }
-  }
-
-  render() {
-    const { url, name, films, residents, created, edited, ...restProps } =
-      this.props;
-    const transformProps = {
-      ...restProps,
-      created: created.toString().slice(0, 10),
-      edited: edited.toString().slice(0, 10),
+  useEffect(() => {
+    const fetchData = async () => {
+      if (films) {
+        const filmTitles = await transformPropsArrayToString(films, 'title');
+        setFilmTitles(filmTitles);
+      }
+      if (residents) {
+        const residentNames = await transformPropsArrayToString(
+          residents,
+          'name'
+        );
+        setResidentNames(residentNames);
+      }
     };
 
-    return (
-      <>
-        <h3 className="card__title">
-          Planet: <i>{name}</i>
-        </h3>
-        {Object.keys(transformProps).map((key) => {
-          const k = key as keyof typeof transformProps;
-          return (
-            <p key={k}>
-              <b>{k}</b>: {String(transformProps[k])}
-            </p>
-          );
-        })}
-        {!!films?.length && (
-          <p>
-            <b>films:</b> [{this.state.filmTitles}]
+    fetchData();
+  });
+
+  return (
+    <>
+      <h3 className="card__title">
+        Planet: <i>{name}</i>
+      </h3>
+      {Object.keys(transformProps).map((key) => {
+        const k = key as keyof typeof transformProps;
+        return (
+          <p key={k}>
+            <b>{k}</b>: {String(transformProps[k])}
           </p>
-        )}
-        {!!residents?.length && (
-          <p>
-            <b>residents:</b> [{this.state.residentNames}]
-          </p>
-        )}
-        {!!url && <a href={url}>link</a>}
-      </>
-    );
-  }
+        );
+      })}
+      {!!films?.length && (
+        <p>
+          <b>films:</b> [{filmTitles}]
+        </p>
+      )}
+      {!!residents?.length && (
+        <p>
+          <b>residents:</b> [{residentNames}]
+        </p>
+      )}
+      {!!url && <a href={url}>link</a>}
+    </>
+  );
 }

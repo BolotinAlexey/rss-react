083f0665ee6f55b510905d6423701068a0866138
Oleksii Bolotin <samkiral73@gmail.com>
2024-07-11 14:12:22 +0200

    feat: add start routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Main from './components/Main';
 
 function App() {
-  return <Main />;
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;

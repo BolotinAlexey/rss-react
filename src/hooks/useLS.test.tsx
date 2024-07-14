2301243740c2d0e9ad15a087f4693a0c9d95e209
Oleksii Bolotin <samkiral73@gmail.com>
2024-07-14 21:57:28 +0200

    test: add tests for useLs and Form search

---
+++ src/hooks/useLS.test.tsx
@@ -0,0 +1,25 @@
+import { render } from '@testing-library/react';
+import useLS from './useLS';
+import { vi } from 'vitest';
+
+vi.mock('react-router-dom', () => ({
+  useLocation: vi.fn(() => ({
+    search: '?page=1&search=test',
+  })),
+}));
+
+describe('useLS custom hook', () => {
+  it('should initialize with value from query parameter', () => {
+    const mockOnSubmitName = vi.fn();
+
+    const TestComponent = () => {
+      const [name] = useLS(mockOnSubmitName);
+      return <div>{name}</div>;
+    };
+
+    const { getByText } = render(<TestComponent />);
+
+    expect(getByText('test')).toBeInTheDocument();
+    expect(mockOnSubmitName).toHaveBeenCalledWith('test');
+  });
+});

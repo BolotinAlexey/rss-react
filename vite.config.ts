963615d0e0674e3aea57aabddf3b65935402b214
Oleksii Bolotin <samkiral73@gmail.com>
2024-07-14 23:53:10 +0200

    feat: finish tests

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
       reporter: ['text', 'json', 'html'],
       all: true,
       include: ['src/**/*.ts', 'src/**/*.tsx'],
-      exclude: ['node_modules', 'dist'],
+      exclude: ['node_modules', 'dist', 'src/service/loaders.ts'],
       thresholds: {
         statements: 80,
         branches: 80,

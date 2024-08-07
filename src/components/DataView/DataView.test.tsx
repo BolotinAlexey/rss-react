d828286313b243f0718fbf791eae9b7eeb0d5d68
Oleksii Bolotin <samkiral73@gmail.com>
2024-08-07 02:00:31 +0200

    test: add tests for DataView.tsx

--- src/components/DataView/DataView.test.tsx
+++ src/components/DataView/DataView.test.tsx
@@ -1,66 +1,109 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
 import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import DataView from './DataView';
 import { Provider } from 'react-redux';
-import store from '../../store';
-import { mockPlanet1, mockPlanet2 } from '../../tests/mockData';
+import { configureStore } from '@reduxjs/toolkit';
+import cardsSlice, { CardsState } from '../../store/slices/cardsSlice';
+import currentCardSlice, {
+  CurrentCardState,
+} from '../../store/slices/currentCardSlice';
+import { mockPlanet1, mockPlanet2, mockRouter } from '../../tests/mockData';
+import { IPlanet } from '../../interfaces';
 
-const planets = [mockPlanet1, mockPlanet2];
+const planets: IPlanet[] = [mockPlanet1, mockPlanet2];
+vi.mock('next/router', () => ({
+  useRouter: () => mockRouter,
+}));
+
+const initialCardsState: CardsState = {
+  selectedCards: [],
+};
+const initialCurrentCardState: CurrentCardState = {
+  currentCard: null,
+};
+
+const renderWithStore = (initialState: {
+  cards: CardsState;
+  currentCard: CurrentCardState;
+}) => {
+  const store = configureStore({
+    reducer: {
+      currentCard: currentCardSlice,
+      cards: cardsSlice,
+    },
+    preloadedState: initialState,
+  });
+
+  return render(
+    <Provider store={store}>
+      <DataView planets={planets} />
+    </Provider>
+  );
+};
+
 describe('DataView', () => {
-  it("displays 'Loading..' when loading", async () => {
-    render(
-      <Provider store={store}>
-        <DataView planets={planets} />
-      </Provider>
-    );
-    expect(screen.getByText('Loading..')).toBeInTheDocument();
-  });
+  it('renders planets with relevant data', async () => {
+    renderWithStore({
+      cards: initialCardsState,
+      currentCard: initialCurrentCardState,
+    });
 
-  it('renders a Tatooine and Alderaan planets with relevant data', async () => {
-    render(
-      <Provider store={store}>
-        <DataView planets={planets} />
-      </Provider>
-    );
-    setTimeout(async () => {
-      await waitFor(() => {
-        expect(screen.getByText('Tatooine')).toBeInTheDocument();
-        expect(screen.getByText('Alderaan')).toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.getByText('Tatooine')).toBeInTheDocument();
+      expect(screen.getByText('Alderaan')).toBeInTheDocument();
+    });
+
+    const user = userEvent.setup();
+    const tatooine = screen.getByText('Tatooine');
+
+    user.click(tatooine);
+
+    await waitFor(() => {
+      expect(mockRouter.push).toHaveBeenCalledWith({
+        pathname: '/',
+        query: { details: '1' },
       });
-      const user = userEvent.setup();
-      const tatooine = screen.getByText('Tatooine');
-
-      user.click(tatooine);
-
-      await waitFor(() => expect(window.location.pathname).toBe(`/details/1/`));
-    }, 1000);
+    });
   });
 
-  it('renders the specified number of planets', async () => {
-    render(
-      <Provider store={store}>
-        <DataView planets={planets} />
-      </Provider>
-    );
+  it('renders the correct number of planets', async () => {
+    renderWithStore({
+      cards: initialCardsState,
+      currentCard: initialCurrentCardState,
+    });
 
-    setTimeout(async () => {
-      await waitFor(() => {
-        const characterList = screen.getAllByRole('listitem');
-        expect(characterList).toHaveLength(10);
+    await waitFor(() => {
+      const planetList = screen.getAllByRole('listitem');
+      expect(planetList).toHaveLength(planets.length);
+    });
+  });
+
+  it("displays 'Not found' when there are no planets", () => {
+    const renderWithStoreEmpty = (initialState: {
+      cards: CardsState;
+      currentCard: CurrentCardState;
+    }) => {
+      const store = configureStore({
+        reducer: {
+          currentCard: currentCardSlice,
+          cards: cardsSlice,
+        },
+        preloadedState: initialState,
       });
-    }, 1000);
-  });
 
-  it("displays 'Not found' when there are no results", async () => {
-    render(
-      <Provider store={store}>
-        <DataView planets={planets} />
-      </Provider>
-    );
-    await waitFor(() =>
-      expect(screen.getByText('Not found')).toBeInTheDocument()
-    );
+      return render(
+        <Provider store={store}>
+          <DataView planets={[]} />
+        </Provider>
+      );
+    };
+    renderWithStoreEmpty({
+      cards: initialCardsState,
+      currentCard: initialCurrentCardState,
+    });
+
+    expect(screen.getByText('Not found')).toBeInTheDocument();
   });
 });

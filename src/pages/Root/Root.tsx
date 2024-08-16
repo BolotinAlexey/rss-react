e73ac59d25cc49315ae0aee81d8cd42ea333f8d4
Oleksii Bolotin <samkiral73@gmail.com>
2024-08-16 13:12:29 +0200

    feat: add basic styles

--- src/pages/Root/Root.tsx
+++ src/pages/Root/Root.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../../components/Header';
+import './root.css';
 
 export default function Root() {
   return (
     <>
       <Header />
       <h1 className="title">Forms App</h1>
-      <main>
+      <main className="container">
         <Outlet />
       </main>
     </>
